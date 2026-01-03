6ca77069f50838433819b2cc1e63b6c16f4fd69f
William Van Uitert <william.vanuitert@gmail.com>
2026-01-03 13:45:07 -0500

    stuff for the image not loading on website

--- public/app.js
+++ public/app.js
@@ -422,8 +422,7 @@ experimentApp.controller('ExperimentController',
     };
 
     $scope.stimuli_sets = [
-      [0]
-      // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     ]
 
     $scope.stimuli_set_length = $scope.stimuli_sets[0].length;
